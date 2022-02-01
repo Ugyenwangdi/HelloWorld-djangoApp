b10246f0155dec0409567366903214c5d3d2be82
Ugyenwangdi <57824653+Ugyenwangdi@users.noreply.github.com>
2022-02-01 12:12:47 +0600

    Update settings.py

--- HelloWorld/HelloWorld/settings.py
+++ HelloWorld/HelloWorld/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%vo5*j&b%p%hwdhocqoeqj30rr8x#bwene6b)&0vhbi9#o0x#a'
+SECRET_KEY = 'YourSecretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
